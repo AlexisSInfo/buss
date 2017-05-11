a78c87385d0ff097dd2692f8c64de052f092d44b
alexis <a.sanhueza09@ufromail.cl>
2017-05-11 00:17:07 -0300

    pequeño update

--- src/main/java/ventanas/Busf.java
+++ src/main/java/ventanas/Busf.java
@@ -1,29 +1,16 @@
 
 package ventanas;
-import java.awt.Container;
-import java.awt.Panel;
+
 import java.io.IOException;
 
 import javax.swing.*;
 
 
-public class Busf extends JFrame {
-
-   
-   
+public class Busf extends JFrame{
     
     public static void main(String[] args) throws IOException {
         
-        
-        //PanelBotones pb = new PanelBotones();
-        //pb.setVisible(true);
-        //pb.im.setVisible(true);
         VentanaPrincipal ventana = new VentanaPrincipal();
-        
-        
-       
-       
-        
         
     }
 }
